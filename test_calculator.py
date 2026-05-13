3fca7c5bcfec93ad0c499c6590ebd1a18df2a18c
cy06666 <1420977954@qq.com>
2026-05-13 17:38:10 +0800

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import add, sub, mul, div
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_sub():
+    assert sub(5, 3) == 2
+
+def test_mul():
+    assert mul(2, 4) == 8
+
+def test_div():
+    assert div(6, 2) == 3
