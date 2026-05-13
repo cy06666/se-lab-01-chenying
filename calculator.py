bc0c613358180711f9fbe42145d76e001c2eeb3d
cy06666 <1420977954@qq.com>
2026-05-13 17:37:50 +0800

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# 计算器功能
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ValueError("除数不能为0")
+    return a / b
